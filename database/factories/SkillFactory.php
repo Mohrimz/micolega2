03e617058bda9609361db8512329d97647ba3a16
FathimaShums <fathimashums416@gmail.com>
2024-12-02 10:31:12 +0530

    testing- created factories for models

---
+++ database/factories/SkillFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+// database/factories/SkillFactory.php
+
+namespace Database\Factories;
+
+use App\Models\Skill;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Skill>
+ */
+class SkillFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Skill::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'category_id' => Category::factory(), 
+        ];
+    }
+}
