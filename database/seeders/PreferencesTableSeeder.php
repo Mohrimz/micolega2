5101131de6d5368eaba014880b2757c79cbd2ed8
FathimaShums <fathimashums416@gmail.com>
2024-12-25 14:54:19 +0530

    Seeded preferences values

---
+++ database/seeders/PreferencesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PreferencesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('preferences')->insert([
+            ['name' => 'group-session', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'one-one session', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
+
